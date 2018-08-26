2351978ba1e0306817cef03c0c78e0a7ecf91d46
Robert Schäfer <git@roschaefer.de>
2018-08-26 15:01:55 +0200

    Fix issues with app-nav template
    * Use link-to helper if possible
    * Update bulma
    * Show menu on mobiles (<1024px)
    * Fix styling of Login/Logout button

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -6,25 +6,10 @@ export default Component.extend({
   session: service(),
   auth0: service(),
   actions: {
-
-    /**
-   * From service/auth, starting the login process
-   */
     login() {
       this.get('auth0.webAuth').authorize();
     },
 
-    goHome() {
-      this.get('router').transitionTo('home');
-    },
-
-    goDashboard() {
-      this.get('router').transitionTo('dashboard');
-    },
-
-    /**
-   * From service/auth, removing the saved token from the session.
-   */
     logout() {
       this.get('session').invalidate()
     }
